097a1cbd13da7627557764f5734700a624a3e0d6
nikcladis <nikcladis@gmail.com>
2024-01-02 23:21:50 +0200

    added Modals, AuthModal, Login

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import AuthModal from "../../components/Modals/AuthModal";
 import Navbar from "../../components/Navbar/Navbar";
 
 type AuthPageProps = {};
@@ -12,6 +13,7 @@ const AuthPage: React.FC<AuthPageProps> = () => {
         <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
           <Image src="/hero.png" alt="Hero img" width={600} height={400} />
         </div>
+        <AuthModal />
       </div>
     </div>
   );
