bc3f803e4dae75f5911168960eebbf28e0d1aa96
nikcladis <nikcladis@gmail.com>
2024-01-03 18:20:24 +0200

    added useCloseModal hook to close modal when user presses out of the modal or presses escape

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { authModalState } from "../../atoms/authModalAtom";
@@ -10,15 +10,14 @@ type AuthModalProps = {};
 
 const AuthModal: React.FC<AuthModalProps> = () => {
   const authModal = useRecoilValue(authModalState);
-  const setAuthModalState = useSetRecoilState(authModalState);
-
-  const handleClose = () => {
-    setAuthModalState((prev) => ({ ...prev, isOpen: false, type: "login" }));
-  };
+  const closeModal = useCloseModal();
 
   return (
     <>
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"></div>
+      <div
+        onClick={closeModal}
+        className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"
+      ></div>
       <div className="w-full sm:w-[450px]  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center">
         <div className="relative w-full h-full mx-auto flex items-center justify-center">
           <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6">
@@ -27,7 +26,7 @@ const AuthModal: React.FC<AuthModalProps> = () => {
                 type="button"
                 className="bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white"
               >
-                <IoClose onClick={handleClose} className="h-5 w-5" />
+                <IoClose onClick={closeModal} className="h-5 w-5" />
               </button>
             </div>
             {authModal.type === "login" && <Login />}
@@ -40,3 +39,24 @@ const AuthModal: React.FC<AuthModalProps> = () => {
   );
 };
 export default AuthModal;
+
+function useCloseModal() {
+  const setAuthModalState = useSetRecoilState(authModalState);
+
+  const closeModal = () => {
+    setAuthModalState((prev) => ({ ...prev, isOpen: false }));
+  };
+
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    window.addEventListener("keydown", handleEscape);
+    return () => window.removeEventListener("keydown", handleEscape);
+  }, []);
+
+  return closeModal;
+}
