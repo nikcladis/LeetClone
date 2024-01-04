f393ae4b496b51d163388054b6fa8d2588a7ab27
nikcladis <nikcladis@gmail.com>
2024-01-04 22:23:19 +0200

    updated Topbar auth functionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/firebase";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+  const [user] = useAuthState(auth);
+
   return (
-    <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
+    <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7 ">
       <div
         className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}
       >
@@ -14,13 +18,13 @@ const Topbar: React.FC<TopbarProps> = () => {
           <Image
             src="/logo-full.png"
             alt="Logo"
-            className="h-full"
+            className="h-full ml-14"
             width={100}
             height={100}
           />
         </Link>
 
-        <div className="flex items-center space-x-4 flex-1 justify-end">
+        <div className="flex items-center space-x-4 flex-1 justify-end mr-14">
           <div>
             <a
               href="https://www.buymeacoffee.com/burakorkmezz"
@@ -31,11 +35,30 @@ const Topbar: React.FC<TopbarProps> = () => {
               Premium
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {!user && (
+            <Link href="/auth">
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          )}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <Image
+                src="/avatar.png"
+                alt="user profile img"
+                className="rounded-full"
+                height={30}
+                width={30}
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
+		transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
